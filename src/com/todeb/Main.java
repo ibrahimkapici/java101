416ea743149fb6b2edc715a4d70966a22d778af8
ibrahimkapici <ibrhm.kapici@gmail.com>
2022-06-27 10:36:07 +0300

    homework7

--- src/com/todeb/Main.java
+++ src/com/todeb/Main.java
@@ -9,12 +9,26 @@ public class Main {
 
     public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Lütfen boyunuzu (metre cinsinden) giriniz:");
-		double length = kb.nextDouble();
-		System.out.println("Lütfen kilonuzu (kg) giriniz");
-		double kg = kb.nextDouble();
-		double endex = kg / pow(length, 2);
-		System.out.println("Vücüt kitle endexiniz: " + endex + "'tir.");
+		System.out.println("Lütfen kaç kg armut aldığınızı giriniz:");
+		double kgArm = kb.nextDouble();
+		System.out.println("Lütfen kaç kg elma aldığınızı giriniz:");
+		double kgElm = kb.nextDouble();
+		System.out.println("Lütfen kaç kg domates aldığınızı giriniz:");
+		double kgDom = kb.nextDouble();
+		System.out.println("Lütfen kaç kg muz aldığınızı giriniz:");
+		double kgMuz = kb.nextDouble();
+		System.out.println("Lütfen kaç kg patlıcan aldığınızı giriniz:");
+		double kgPat = kb.nextDouble();
+
+		double priceArm = 2.14;
+		double priceElm = 3.67;
+		double priceDom = 1.11;
+		double priceMuz = 0.95;
+		double pricePat = 5.00;
+		double totalPrice;
+		totalPrice = (kgArm * priceArm) + (kgElm * priceElm) + (kgDom * priceDom) + (kgMuz * priceMuz) + (kgPat * pricePat);
+		System.out.println("Toplam tutar:" + totalPrice + "TL");
+
     }
 
 }
