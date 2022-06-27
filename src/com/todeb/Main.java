42e6ab78f5fbe437ef3d2435c905cf101e5cb658
ibrahimkapici <ibrhm.kapici@gmail.com>
2022-06-27 10:28:12 +0300

    homework6

--- src/com/todeb/Main.java
+++ src/com/todeb/Main.java
@@ -9,14 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Lütfen dairenin yarıçapını giriniz:");
-		double r = kb.nextDouble();
-		System.out.println("Lütfen dairenin açışını giriniz");
-		double deg = kb.nextDouble();
-		double area = 3.14 * pow(r, 2);
-		double cevre = 2 * 3.14 * r;
-		deg *= area/360;
-		System.out.println(deg);
+		System.out.println("Lütfen boyunuzu (metre cinsinden) giriniz:");
+		double length = kb.nextDouble();
+		System.out.println("Lütfen kilonuzu (kg) giriniz");
+		double kg = kb.nextDouble();
+		double endex = kg / pow(length, 2);
+		System.out.println("Vücüt kitle endexiniz: " + endex + "'tir.");
     }
 
 }
