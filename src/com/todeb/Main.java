b0af6a11f8582e6412e2f222d3eb181783bd479d
ibrahimkapici <ibrhm.kapici@gmail.com>
2022-06-27 10:13:51 +0300

    homework4

--- src/com/todeb/Main.java
+++ src/com/todeb/Main.java
@@ -9,41 +9,19 @@ public class Main {
 
     public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Lütfen ilk kenar uzunluğunu giriniz:");
-		double len1 = kb.nextDouble();
-		System.out.println("Lütfen ikinci kenar uzunluğunu giriniz:");
-		double len2 = kb.nextDouble();
-		double hippow2;
-		hippow2 = pow(len1, 2) + pow(len2, 2);
-		double hip = sqrt(hippow2);
-		System.out.println(hip + " Üçgenin hipotenus uzunluğudur.");
-
-
-
-/*	Scanner kb = new Scanner(System.in);
-	System.out.println("Not ortalaması almak için lütfen derslerin gerekli not puanlarını giriniz:");
-
-	System.out.println("Matematik dersinin notunu giriniz:");
-	int mat = kb.nextInt();
-
-	System.out.println("Fizik dersinin notunu giriniz:");
-	int fizik = kb.nextInt();
-
-	System.out.println("Kimya dersinin notunu giriniz:");
- 	int kimya = kb.nextInt();
-
- 	System.out.println("Tarih dersinin notunu giriniz:");
- 	int tarih = kb.nextInt();
-
- 	System.out.println("Müzik dersinin notunu giriniz:");
- 	int muzik = kb.nextInt();
-
- 	System.out.println("Türkçe dersinin notunu giriniz:");
- 	int turkce = kb.nextInt();
-
- 	int ortalama = (mat + fizik + kimya + tarih + muzik + turkce) / 6;
-
-    System.out.println(ortalama > 60 ? "Sınıfı geçti": "Sınıfta kaldı");*/
+		System.out.println("Lütfen kaç km yol gittiğinizi giriniz:");
+		double km = kb.nextDouble();
+		double price = 20.0;
+		double openPrice = 10.0;
+		double pricePerKm = 2.20;
+		double totalPrice;
+		totalPrice = openPrice + (km * pricePerKm);
+		if(totalPrice <= 20) {
+			System.out.println("Toplam fiyat" + 20 + "TL.");
+		}
+		else {
+			System.out.println("Toplam fiyat" + totalPrice + "TL.");
+		}
     }
 
 }
