abc439f66e4423224fb3acef8cb05ca6e92ecb5d
ibrahimkapici <ibrhm.kapici@gmail.com>
2022-06-27 12:12:11 +0300

    homework9

--- src/com/todeb/Main.java
+++ src/com/todeb/Main.java
@@ -7,25 +7,33 @@ import static java.lang.Math.sqrt;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Lütfen ilk sayıyı giriniz:");
-		int num1 = kb.nextInt();
-		System.out.println("Lütfen ikinci sayıyı giriniz:");
-		int num2 = kb.nextInt();
-		System.out.println("Lütfen yapmak istediğiniz işlemi giriniz: 1- toplam, 2-cıkarma, 3-çarpma, 4-bölme");
-		int select = kb.nextInt();
+		System.out.println("Lütfen kullanıcı adınızı giriniz:");
+		String id = kb.nextLine();
+		System.out.println("Lütfen kullanıcı şifrenizi giriniz:");
+		String pass = kb.nextLine();
 
-		switch (select){
-			case 1: System.out.println("İşlemin sonucu " + (num1 + num2));
+		if (id.equals("patika") && pass.equals("java123")) {
+			System.out.print("Giriş başarılı");
+		} else {
+			System.out.print("Kullancı adı ya da şifre hatalı. Şifre sıfırlamak ister misiniz? Select 1-Evet 2-Hayır");
+			int select = kb.nextInt();
+			switch (select) {
+				case 1:
+					Scanner kbd = new Scanner(System.in);
+					System.out.println("Lütfen yeni bir şifre giriniz:");
+					String newPass = kbd.nextLine();
+					if (newPass.equals(pass)) {
+						System.out.println("Yeni şifre ile eski şifre aynı olamaz. Lütfen başka bir şifre giriniz.");
+					} else
+						System.out.println("Şifre başarılı bir şekilde değiştirildi.");
+
 					break;
-			case 2:System.out.println("İşlemin sonucu " + (num1 - num2));
-					break;
-			case 3:System.out.println("İşlemin sonucu " + (num1 * num2));
-					break;
-			case 4:System.out.println("İşlemin sonucu " + (num1 / num2));
-					break;
+
+				case 2:
+					System.out.println("Lütfen şifrenizi kontrol edip tekrar login olunuz.");
+			}
 		}
 	}
-
 }
