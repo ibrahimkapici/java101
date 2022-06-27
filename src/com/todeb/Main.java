c21199e22c382412d4d904bc236ea976ba397278
ibrahimkapici <ibrhm.kapici@gmail.com>
2022-06-27 11:05:47 +0300

    homework8

--- src/com/todeb/Main.java
+++ src/com/todeb/Main.java
@@ -9,26 +9,23 @@ public class Main {
 
     public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Lütfen kaç kg armut aldığınızı giriniz:");
-		double kgArm = kb.nextDouble();
-		System.out.println("Lütfen kaç kg elma aldığınızı giriniz:");
-		double kgElm = kb.nextDouble();
-		System.out.println("Lütfen kaç kg domates aldığınızı giriniz:");
-		double kgDom = kb.nextDouble();
-		System.out.println("Lütfen kaç kg muz aldığınızı giriniz:");
-		double kgMuz = kb.nextDouble();
-		System.out.println("Lütfen kaç kg patlıcan aldığınızı giriniz:");
-		double kgPat = kb.nextDouble();
+		System.out.println("Lütfen ilk sayıyı giriniz:");
+		int num1 = kb.nextInt();
+		System.out.println("Lütfen ikinci sayıyı giriniz:");
+		int num2 = kb.nextInt();
+		System.out.println("Lütfen yapmak istediğiniz işlemi giriniz: 1- toplam, 2-cıkarma, 3-çarpma, 4-bölme");
+		int select = kb.nextInt();
 
-		double priceArm = 2.14;
-		double priceElm = 3.67;
-		double priceDom = 1.11;
-		double priceMuz = 0.95;
-		double pricePat = 5.00;
-		double totalPrice;
-		totalPrice = (kgArm * priceArm) + (kgElm * priceElm) + (kgDom * priceDom) + (kgMuz * priceMuz) + (kgPat * pricePat);
-		System.out.println("Toplam tutar:" + totalPrice + "TL");
-
-    }
+		switch (select){
+			case 1: System.out.println("İşlemin sonucu " + (num1 + num2));
+					break;
+			case 2:System.out.println("İşlemin sonucu " + (num1 - num2));
+					break;
+			case 3:System.out.println("İşlemin sonucu " + (num1 * num2));
+					break;
+			case 4:System.out.println("İşlemin sonucu " + (num1 / num2));
+					break;
+		}
+	}
 
 }
